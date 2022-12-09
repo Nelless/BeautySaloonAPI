5bc1513a1eb6bf486f76e9378ad3bc6ba94a97dc
Nelless <lego_movie@mail.ru>
2022-12-09 11:34:27 +0500

    API Project created

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BeautySaloonAPI.Models;
+
+namespace BeautySaloonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly BeautysaloonContext _context;
+
+        public ServicesController(BeautysaloonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Services
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Services>>> GetServices()
+        {
+            return await _context.Services.ToListAsync();
+        }
+
+        // GET: api/Services/5
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<IEnumerable<Services>>> GetServices(int Id)
+        {
+            var services = await _context.Services.Where(x => x.Id == Id).ToListAsync();
+
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            return services;
+        }
+
+        // PUT: api/Services/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> PutServices(int Id, Services services)
+        {
+            if (Id != services.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(services).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServicesExists(Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Services
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Services>> PostServices(Services services)
+        {
+            _context.Services.Add(services);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServices", new { id = services.Id }, services);
+        }
+
+        // DELETE: api/Services/5
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<Services>> DeleteServices(int Id)
+        {
+            var services = await _context.Services.FindAsync(Id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            _context.Services.Remove(services);
+            await _context.SaveChangesAsync();
+
+            return services;
+        }
+
+        private bool ServicesExists(int id)
+        {
+            return _context.Services.Any(e => e.Id == id);
+        }
+    }
+}
